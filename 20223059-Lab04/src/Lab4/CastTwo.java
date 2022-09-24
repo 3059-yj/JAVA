44447dff571fd627e974f463c947a9defd8c4f88
3059-yj <105334971+3059-yj@users.noreply.github.com>
2022-09-25 01:13:42 +0900

    객지프 Lab04 과제

---
+++ 20223059-Lab04/src/Lab4/CastTwo.java
@@ -0,0 +1,39 @@
+package Lab4;
+
+class CastTwo {
+	public static void main(String args[]) {
+		byte byte1 ;
+		short short1;
+		char char1 = '\ud55c';
+		int int1;
+		float float1 = 3.6f;
+		
+		// char => byte => char
+		byte1 = (byte) char1;
+		System.out.print("char1 = "+char1 + " =>\t byte1 = "+byte1);
+		System.out.println(" => \t(char)"+byte1 +" = "+(char) byte1);
+		
+		// char => short => char
+		short1 = (short) char1;
+		System.out.print("char1 = "+char1 + " =>\t short1 = "+short1);
+		System.out.println(" => \t(char)"+short1 +" = "+(char) short1);
+		
+		//char => int => char
+		int1 = char1;
+		System.out.print("char1 = "+char1 + " =>\t int1 = "+int1);
+		System.out.println(" => \t(char)"+int1 +" = "+(char) int1);
+		
+
+		//float => int => float
+		int1 = (int) float1;
+		// float(32 bits) => int(32 bits)
+		System.out.print("float1 = "+float1 + " => \t int1 = "+int1);
+		System.out.println(" => \t(float)"+int1 +" = "+(float) int1);
+
+		//short => byte
+		short1 = -134;
+		byte1 = (byte) short1;
+		System.out.println("short1 = "+short1 + " => \t byte1 = "+byte1);
+
+}
+}
---
+++ 20223059-Lab04/src/Lab4/DoubleTester.java
@@ -0,0 +1,12 @@
+package Lab4;
+
+public class DoubleTester 
+{ 
+    public static void main(String args[]) 
+    { 
+       double probability = 8.70; 
+       int percentage = (int) (100 * probability); 
+       System.out.println(percentage); 
+//       System.out.println(Math.round(probability * 100));
+    } 
+}
